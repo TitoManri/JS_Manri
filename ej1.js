da859337aa54aeafceb98cb5186aac0beea08c12
Tito <61942255+TitoManri@users.noreply.github.com>
2023-05-25 19:10:27 -0600

    Update ej1.js

--- ej1.js
+++ ej1.js
@@ -1,1 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.sc303.clase2;
+
+/**
+ *
+ * @author Laboratorios
+ */
+public class Clase2 {
+
+    public static void main(String[] args) {
+        /* Variables */
+        int num1 = 4;
+        int num2 = 2;
+        int num3 = 3; 
+        int num4 = 4;
+        
+        /* Formula */
+        float suma = (num1 + num2 + num3 + num4);
+        float promedio = suma / 4;
+          
+        /* Imprimir el resultado */
+        System.out.println("Respuesta de la suma: "+suma);
+        System.out.println("El promedio es: "+ promedio);
+        
+        
+        
+    }
+}
 
