c5ae3a9852ea3903f043ec957f5297aac8137cdb
Tito <61942255+TitoManri@users.noreply.github.com>
2023-05-25 20:40:14 -0600

    Update ej3.js

--- ej3.js
+++ ej3.js
@@ -1,1 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.sc303.clase2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Laboratorios
+ */
+public class Clase2 {
+
+    public static void main(String[] args) {
+        /* Variables */
+        String respuesta = JOptionPane.showInputDialog("Escribe tu nombre");
+        
+        
+        
+        /* Formula */
+        
+          
+        /* Imprimir el resultado */
+        JOptionPane.showMessageDialog(null,"Hola " +respuesta+ " bienvenid@ a este programa desarrollado en JAVA con Netbeans.");
+
+        System.out.println(respuesta);
+        
+        
+        
+    }
+}
 
