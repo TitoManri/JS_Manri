bfb808bf41f6f3e5fbb63b86de35a2addf51c54b
Tito <61942255+TitoManri@users.noreply.github.com>
2023-06-08 20:12:46 -0600

    Update and rename Tarea to tarea.js

---
+++ tarea.js
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.sc303.clase4;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Laboratorios
+ */
+public class Clase4 {
+
+    public static void main(String[] args) {
+        int estudiantes = 0;
+        int nota_mayor = 0;
+        int nota_menor = 100;
+        
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de estudiantes: "));
+        cantidad = estudiantes;
+        while (estudiantes == 0){
+        int nota = Integer.parseInt(JOptionPane.showInputDialog("Digite su nota: "));
+        
+        
+        if(nota >= 70 && nota <= 100){
+            JOptionPane.showMessageDialog(null,"Aprovo");
+        }else if (nota <= 70 && nota >= 0){
+            JOptionPane.showMessageDialog(null,"Reprobo");
+        }
+        if(nota >= nota_mayor){
+            nota = nota_mayor;
+        }else if (nota <= nota_menor){
+            nota = nota_menor;
+        }
+                estudiantes--;
+        
+        
+        }
+        JOptionPane.showMessageDialog(null,"La nota Mayor es: "+nota_mayor+"\n La nota menor es: "+nota_menor+"");
+        
+        }
+        
+    }
+
